279cf284299c17284425c73cbca82696c5e59812
lukegjpotter <lukegjpotter@gmail.com>
2025-12-15 20:21:06 +0000

    Initial Commit

---
+++ src/main/java/com/test/travis_ci_demo/TravisCiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.test.travis_ci_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravisCiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravisCiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/travis_ci_demo/TravisCiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.travis_ci_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravisCiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
